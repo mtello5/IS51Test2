ac3038034dcc6f7fa7f363bc62515326f0eee627
mtello5 <mtello5@mail.fresnostate.edu>
2021-04-29 17:44:09 -0700

    added structured-english

---
+++ IS51Test2.py
@@ -0,0 +1,12 @@
+"""
+This program will determine the amount of grades, the average grade, and the percentage of grades above the average grade on a final exam.
+
+Text file will be read and made into a list of intergers
+
+Number of grades = length of list of intergers
+
+Average grade = sum of grades / # of total grades
+
+Percentage of grades above average = grades > average / # of total grades 
+
+"""
